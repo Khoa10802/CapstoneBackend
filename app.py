3323f9fba0f9c4deb81a732032e6832884294f21
Khoa10802 <s3884419@rmit.edu.vn>
2025-08-18 08:58:07 +0700

    add ai model

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from flask_cors import CORS
 from solcx.exceptions import SolcError, UnsupportedVersionError
 
 from Compilation.sol_compilation import package_assemble, ExternalInclusionError, VersionNotFoundError
+from predict import predict_vulnerabilities
 
 app = Flask(__name__, template_folder="templates")
 CORS(app)
@@ -32,12 +33,13 @@ def upload():
                 docx_text.append(para.text)
             if len(docx_text) <= 1 and docx_text[0] == '':
                 return jsonify({"error": f"{sol_file.filename} is maybe empty"}), 400
-            return package_assemble('\n'.join(docx_text))
+            compiled_sol = package_assemble('\n'.join(docx_text))
         else:
             sol_str = sol_file.read().decode('utf-8')
             if len(sol_str) <= 0:
                 return jsonify({"error": f"{sol_file.filename} is maybe empty"}), 400
-            return package_assemble(sol_str)
+            compiled_sol = package_assemble(sol_str)
+        return predict_vulnerabilities(compiled_sol)
 
     except ExternalInclusionError:
         return jsonify({"error": f"{sol_file.filename} may contain external library"}), 400
