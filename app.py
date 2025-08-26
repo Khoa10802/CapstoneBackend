85c2828978d4ba866b9a64ad0af50eb7d3364db7
Khoa10802 <s3884419@rmit.edu.vn>
2025-08-26 17:07:31 +0700

    move out of own folder

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from docx import Document
 from solcx.exceptions import SolcError, UnsupportedVersionError
 
 # Custom imports
-from Compilation.sol_compilation import package_assemble, ExternalInclusionError, VersionNotFoundError
+from sol_compilation import package_assemble, ExternalInclusionError, VersionNotFoundError
 from predict import predict_vulnerabilities, predict_contract  # Gộp cả 2 hàm predict
 
 app = Flask(__name__, template_folder="templates")
